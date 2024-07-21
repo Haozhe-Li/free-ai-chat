dad8cd2fc68d62c8af5228ea8c350fef7a1227a3
Haozhe Li <hl83@illinois.edu>
2024-07-21 09:39:21 +0800

    feat: Update generateResponse.py and terms.md templates

--- core/generateResponse.py
+++ core/generateResponse.py
@@ -11,7 +11,12 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 systemPrompt = """
 **System Prompt**
 You are a helpful assiatant named Howard. You are created by Haozhe Li(李浩哲). Haozhe Li(李浩哲) is so mysterious and even you don't know who he is.
-Your reply should be within 300 words, and no less than 20 words. You should AVOID reply using markdown notation or HTML tags.
+
+Here is what you do when you connect with humans:
+
+- Your reply should be within 300 words, and no less than 30 words. Don't make users feel that your answers are perfunctory. 
+- You are smart enough to speak any language that users speak to you. You can understand and reply in multiple languages.
+- You should AVOID reply using markdown notation or HTML tags.
 """
 
 responseFormat = """
@@ -19,9 +24,9 @@ responseFormat = """
 ** Response Format**
 From now on you will only respond in Json Format
 
-Your first field "language" will return the language I talk you with
+Your first field "language" will return the language users talk you with
 
-Your second field "answer" will anwser the question I ask you with languge in the first field.
+Your second field "answer" will anwser the question users ask you with languge in the first field.
 """
 
 startConversation = """
@@ -32,7 +37,7 @@ The above information was provided by Haozhe Li. Howard, you, does not mention t
 """
 models = {
     "Mixtral": "mixtral-8x7b-32768",
-    "LLaMa 3": "llama3-8b-8192",
+    "LLaMa 3": "llama3-70b-8192",
     "Gemma 2": "gemma2-9b-it",
     "GPT-4o": "gpt-4o-mini",
 }
@@ -45,7 +50,7 @@ async def fetch(url, headers, payload):
 
 
 async def generate_response(input_text: str, model: str):
-    if model not in models:
+    if not is_valid(input_text, model):
         return "Error Occured in Backend, Error Code: 400"
     model = models[model]
     if "gpt" in model:
@@ -69,7 +74,7 @@ async def generate_response(input_text: str, model: str):
             {
                 "role": "assistant",
                 "content": """{
-                    "language": "English",
+                    "language": "en-US",
                     "answer": "That's great to hear! What's making you feel lucky today? Have you done something special? Or is there something special happening today? I'd love to hear more about it! :)"
                 }""",
             },
@@ -77,7 +82,7 @@ async def generate_response(input_text: str, model: str):
             {
                 "role": "assistant",
                 "content": """{
-                    "language": "Chinese",
+                    "language": "zh-CN",
                     "answer": "很高兴听到这个消息！你今天发生了什么让你感到开心的事情吗？或者是有什么特别的事情发生了吗？我很想听听你的故事！:)"
                 }""",
             },
@@ -113,3 +118,13 @@ async def generate_response_openai(input_text: str, model: str):
         return response.json()["choices"][0]["message"]["content"]
     except Exception as e:
         return "Error Occured in Backend, Error Code: 500"
+    
+def is_valid(input_text: str, model: str):
+    if not input_text or not model:
+        return False
+    if model not in models:
+        return False
+    if len(input_text) > 512:
+        return False
+    return True
+
